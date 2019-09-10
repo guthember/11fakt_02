e729023c3feb75985dc4e5425afadd19864d8e79
Erik Gúth <guthember@gmail.com>
2019-09-10 15:05:58 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElsoProgram
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // változó bevezetése
+      // típus -> String
+      // neve -> mondat
+      // tartalma -> "Hello world!"
+      String nev; // név tárolására
+
+      Console.Write("Írd be a neved: ");
+      nev = Console.ReadLine();
+
+      Console.Write("Hello ");
+      Console.WriteLine(nev);
+
+      Console.ReadKey();
+    }
+  }
+}
